1723a66813ed7c4a01c0277a53cd7bfbb3450f27
tvldev01 <md.ismail@technovista.com.bd>
2016-02-23 12:33:11 +0600

    added logger for service

--- AIMS_BD_IATI.Service/Common.cs
+++ AIMS_BD_IATI.Service/Common.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 //added reference Assemblies->Framework->System.Configuration
 using System.Configuration;
+using System.IO;
 
 namespace AIMS_BD_IATI.Service
 {
@@ -25,6 +26,26 @@ namespace AIMS_BD_IATI.Service
         public static string iati_stream
         {
             get { return ConfigurationManager.AppSettings["iati_stream"]; }
+        }
+    }
+
+    public sealed class Log
+    {
+        public static void Write(string Text)
+        {
+            string dirPath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location) + "\\Log\\";
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(dirPath);
+            if (!directoryInfo.Exists)
+                directoryInfo.Create();
+
+            string filePath = dirPath + DateTime.Now.ToString("yyyy-MM-dd") + "_Log.txt";
+
+            File.AppendAllText(filePath,
+                Environment.NewLine +
+                DateTime.Now.ToString("d-MMM-yyyy, HH:mm:ss") + "  " + Text);
+
+            System.Console.WriteLine(Text);
         }
     }
 }
