bbf90d8f5baced979eaf860e5afaddb0dbdc53e3
tvldev02 <faruque.ahamed@technovista.com.bd>
2016-03-20 10:35:20 +0600

    Included missing page 9TFnCF

---
+++ AIMS_BD_IATI.website/Modules/IATIImport/9TFnCF/9TFnCFController.js
@@ -0,0 +1,13 @@
+﻿angular.module('iatiDataImporter').controller("9TFnCFController", function ($rootScope,$uibModalInstance, $timeout, $scope, $http,Activity,Project) {
+    $scope.Activity = Activity;
+    $scope.Project = Project;
+
+    $scope.ok = function () {
+        $uibModalInstance.close();
+    };
+
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
