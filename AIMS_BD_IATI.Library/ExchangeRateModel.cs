316e3b9307685c971e6e0388f92c4575d5df7bd1
tvldev02 <faruque.ahamed@technovista.com.bd>
2016-03-27 10:12:58 +0600

    Added exchange rate model

---
+++ AIMS_BD_IATI.Library/ExchangeRateModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIMS_BD_IATI.Library
+{
+    public partial class ExchangeRateModel
+    {
+        public System.DateTime DATE { get; set; }
+        public string ISO_CURRENCY_CODE { get; set; }
+        public string CURRENCY_NAME { get; set; }
+        public Nullable<decimal> CURRENCY_PER_TAKA { get; set; }
+        public Nullable<decimal> TAKA_PER_CURRENCY { get; set; }
+        public Nullable<decimal> DOLLAR_PER_CURRENCY { get; set; }
+        public Nullable<decimal> CURRENCY_PER_DOLLAR { get; set; }
+    }
+}
