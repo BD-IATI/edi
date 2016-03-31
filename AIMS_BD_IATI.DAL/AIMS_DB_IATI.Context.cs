f4eecba8200b6102ba6b7d1143fa5cb80870fef5
tvldev01 <md.ismail@technovista.com.bd>
2016-03-31 12:30:01 +0600

    added field mapping for delegated activity

--- AIMS_BD_IATI.DAL/AIMS_DB_IATI.Context.cs
+++ AIMS_BD_IATI.DAL/AIMS_DB_IATI.Context.cs
@@ -28,5 +28,6 @@ namespace AIMS_BD_IATI.DAL
         public virtual DbSet<FieldMappingPreferenceActivity> FieldMappingPreferenceActivities { get; set; }
         public virtual DbSet<FieldMappingPreferenceGeneral> FieldMappingPreferenceGenerals { get; set; }
         public virtual DbSet<Activity> Activities { get; set; }
+        public virtual DbSet<FieldMappingPreferenceDelegated> FieldMappingPreferenceDelegateds { get; set; }
     }
 }
