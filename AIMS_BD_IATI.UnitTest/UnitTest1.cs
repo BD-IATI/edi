80602c08bb47a8438be000580476222d4e14c194
tvldev02 <faruque.ahamed@technovista.com.bd>
2016-04-17 10:31:48 +0600

    added test project

---
+++ AIMS_BD_IATI.UnitTest/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AIMS_BD_IATI.Library;
+using AIMS_BD_IATI.Library.Parser.ParserIATIv2;
+namespace AIMS_BD_IATI.UnitTest
+{
+    [TestClass]
+    public class NullCheckerTest
+    {
+        [TestMethod]
+        public void ReturnTheObjFromSpecifiedPositionOfAnArray()
+        {
+            string expected = "j";
+
+            string[] array = new string[2] { "j", "k" };
+
+            string actual = array.n(0);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ReturnTheObjFromSpecifiedPositionOfAnArray2()
+        {
+            string expected = string.Empty;
+
+            string[] array = null;
+
+            string actual = array.n(0);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ReturnTheObjFromSpecifiedPositionOfAnArray3()
+        {
+
+            iatiactivity[] array = null;
+
+            iatiactivity obj = array.n(0);
+
+
+            Assert.IsNotNull(obj);
+        }
+
+        [TestMethod]
+        public void ReturnTheObjFromSpecifiedPositionOfAnArray4()
+        {
+
+            iatiactivity[] array = null;
+
+            iatiactivity obj = array.n(0);
+
+
+            Assert.IsNotNull(obj);
+        }    
+    }
+}
