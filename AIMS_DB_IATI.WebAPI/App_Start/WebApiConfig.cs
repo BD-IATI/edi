0d57e916c86f385e1564fd723563a837c3ac0c1b
tvldev02 <faruque@technovista.com.bd>
2016-12-08 19:09:38 +0600

    integrated elmah for error log

--- AIMS_DB_IATI.WebAPI/App_Start/WebApiConfig.cs
+++ AIMS_DB_IATI.WebAPI/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Elmah.Contrib.WebApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
 
 namespace AIMS_BD_IATI.WebAPIAPI
 {
@@ -10,7 +12,7 @@ namespace AIMS_BD_IATI.WebAPIAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config.Services.Add(typeof(IExceptionLogger), new ElmahExceptionLogger());
             // Web API routes
             config.MapHttpAttributeRoutes();
 
