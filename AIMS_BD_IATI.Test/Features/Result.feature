6aa8dbc0fca1801857d661c81775b1f5de003c7b
dfaruque <dfaruque@gmail.com>
2018-03-13 10:33:20 +0600

    added Resust Tests

---
+++ AIMS_BD_IATI.Test/Features/Result.feature
@@ -0,0 +1,11 @@
+﻿Feature: Results
+
+  Scenario: User can see results in the IATI import interface
+    Given User uses the IATI import module to import a project
+    And User proceeds to the `5. Set import preferences` step
+    Then the page includes the number of `/result/indicator` (e.g. "5 results")
+
+  Scenario: User can import results data
+    Given User imports a project from IATI data
+    And the project contains at least one `result/indicator`
+    Then on the `Results` tab the table contains at least one indicator.
---
+++ AIMS_BD_IATI.Test/Features/ResultSteps.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using TechTalk.SpecFlow;
+
+namespace AIMS_BD_IATI.Test.Features
+{
+    [Binding]
+    public class ResultSteps : SeleniumBase
+    {
+        [Then(@"the page includes the number of `/result/indicator` \(e\.g\. ""(.*)""\)")]
+        public void ThenThePageIncludesTheNumberOfResultIndicatorE_G_(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"the project contains at least one `result/indicator`")]
+        public void GivenTheProjectContainsAtLeastOneResultIndicator()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"on the `Results` tab the table contains at least one indicator\.")]
+        public void ThenOnTheResultsTabTheTableContainsAtLeastOneIndicator_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+
+    }
+}
