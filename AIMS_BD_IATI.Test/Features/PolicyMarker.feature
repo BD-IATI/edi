2cbf5889588929551eabdb1a496b0e29012be255
dfaruque <dfaruque@gmail.com>
2018-03-13 10:29:41 +0600

    added PolicyMarker Tests

---
+++ AIMS_BD_IATI.Test/Features/PolicyMarker.feature
@@ -0,0 +1,11 @@
+﻿Feature: Policy markers from IATI
+
+  Scenario: User can see policy markers in the IATI import interface
+    Given User uses the IATI import module to import a project
+    And User proceeds to the `5. Set import preferences` step
+    Then the page includes the list of policy markers that have a significance code that is not `0`.
+
+  Scenario: User can import a policy marker
+    Given User imports a project from IATI data
+    And the project contains at least one policy marker with a significance code that is not `0`
+    Then on the `Sector Contribution` tab the `Policy Marker` collapsible contains the same policy markers
---
+++ AIMS_BD_IATI.Test/Features/PolicyMarkerSteps.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using TechTalk.SpecFlow;
+
+namespace AIMS_BD_IATI.Test.Features
+{
+    [Binding]
+    public class PolicyMarkerSteps : SeleniumBase
+    {
+        [Given(@"User uses the IATI import module to import a project")]
+        public void GivenUserUsesTheIATIImportModuleToImportAProject()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"User proceeds to the `(.*)\. Set import preferences` step")]
+        public void GivenUserProceedsToThe_SetImportPreferencesStep(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the page includes the list of policy markers that have a significance code that is not `(.*)`\.")]
+        public void ThenThePageIncludesTheListOfPolicyMarkersThatHaveASignificanceCodeThatIsNot_(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"User imports a project from IATI data")]
+        public void GivenUserImportsAProjectFromIATIData()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"the project contains at least one policy marker with a significance code that is not `(.*)`")]
+        public void GivenTheProjectContainsAtLeastOnePolicyMarkerWithASignificanceCodeThatIsNot(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"on the `Sector Contribution` tab the `Policy Marker` collapsible contains the same policy markers")]
+        public void ThenOnTheSectorContributionTabThePolicyMarkerCollapsibleContainsTheSamePolicyMarkers()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+
+    }
+}
